0d6a4478182b22423ae9f8869d62da34905cf86c
tsaripov847-sudo <tsaripov847@gmail.com>
2026-06-11 09:29:19 +0300

    Delete 245 directory

--- 245/3.py
+++
@@ -1,7 +0,0 @@
-while True:
-    password = input("Введите ПИН-КОД: ")
-    if password == "4590":
-        print("Доступ разрешен")
-        break
-    else:
-        print("ПИН-КОд не верный попробуйте еще раз")
